855a9136358198f6e315fe8f584862a13e8e62b8
João Renato Zanetti de Lima <renatoz07@yahoo.com.br>
2021-05-01 14:09:25 -0300

    first commit

---
+++ clojurefunctions/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojurefunctions
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojurefunctions/test/clojurefunctions/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojurefunctions.core-test
+  (:require [clojure.test :refer :all]
+            [clojurefunctions.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
